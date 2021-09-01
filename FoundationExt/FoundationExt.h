b5d5c6d06d1d3eba758b716c396e06935bc0c995
SuXinDe <suxinde2009@126.com>
2021-09-01 11:41:41 +0800

    update

---
+++ FoundationExt/FoundationExt.h
@@ -0,0 +1,18 @@
+//
+//  FoundationExt.h
+//  FoundationExt
+//
+//  Created by SuXinDe on 2021/9/1.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FoundationExt.
+FOUNDATION_EXPORT double FoundationExtVersionNumber;
+
+//! Project version string for FoundationExt.
+FOUNDATION_EXPORT const unsigned char FoundationExtVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FoundationExt/PublicHeader.h>
+
+
