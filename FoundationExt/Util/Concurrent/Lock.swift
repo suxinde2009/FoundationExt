d985a54b8824b39291f0fea3334c5bf373992e5e
suxinde2009 <suxinde2009@126.com>
2021-12-06 22:31:09 +0800

    update

--- FoundationExt/Util/Concurrent/Lock.swift
+++ FoundationExt/Util/Concurrent/Lock.swift
@@ -139,3 +139,64 @@ public extension DispatchQueue {
         tracker.insert(name)
     }
 }
+
+
+public final class RWLock {
+    var mutex = pthread_mutex_t()
+    var cond = pthread_cond_t()
+    var reader = 0
+    var writer = 0
+    var pending = 0
+
+    init() {
+        pthread_mutex_init(&mutex, nil)
+        pthread_cond_init(&cond, nil)
+    }
+
+    deinit {
+        pthread_cond_destroy(&cond)
+        pthread_mutex_destroy(&mutex)
+    }
+
+    func lockRead() {
+        pthread_mutex_lock(&mutex); defer { pthread_mutex_unlock(&mutex) }
+        while writer>0 || pending>0 {
+            pthread_cond_wait(&cond, &mutex)
+        }
+        reader += 1
+    }
+
+    func unlockRead() {
+        pthread_mutex_lock(&mutex); defer { pthread_mutex_unlock(&mutex) }
+        reader -= 1
+        if reader == 0 {
+            pthread_cond_broadcast(&cond)
+        }
+    }
+
+    func lockWrite() {
+        pthread_mutex_lock(&mutex); defer { pthread_mutex_unlock(&mutex) }
+        pending += 1
+        while writer>0||reader>0 {
+            pthread_cond_wait(&cond, &mutex)
+        }
+        pending -= 1
+        writer += 1
+    }
+
+    func unlockWrite() {
+        pthread_mutex_lock(&mutex); defer { pthread_mutex_unlock(&mutex) }
+        writer -= 1
+        pthread_cond_broadcast(&cond)
+    }
+
+    var isWriting: Bool {
+        pthread_mutex_lock(&mutex); defer { pthread_mutex_unlock(&mutex) }
+        return writer>0
+    }
+
+//    var isReading: Bool {
+//        pthread_mutex_lock(&mutex); defer { pthread_mutex_unlock(&mutex) }
+//        return reader>0
+//    }
+}
